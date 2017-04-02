95011e3581bb1ecb3d33c9fb7418a135614f38b4
Myungho <12audgh@naver.com>
2017-04-02 20:01:29 +0900

    [명호] 카드뷰 추가용 FloatingButton 추가

--- myungho/HomeNect/app/src/main/java/com/capstone/sejong/homenect/MainActivity.java
+++ myungho/HomeNect/app/src/main/java/com/capstone/sejong/homenect/MainActivity.java
@@ -1,17 +1,22 @@
 package com.capstone.sejong.homenect;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int REQUEST_ACT = 1000;
+
     private List<Thing> things;
     private RecyclerView rv;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +28,31 @@ public class MainActivity extends AppCompatActivity {
         rv.setLayoutManager(llm);
         rv.setHasFixedSize(true);
 
+        fab = (FloatingActionButton)findViewById(R.id.fab_settings);
+
+        initFab();
         initializeData();
         initializeAdapter();
     }
 
     private void initializeData(){
         things = new ArrayList<>();
-        things.add(new Thing("test", false, R.drawable.default_photo));
+        things.add(new Thing("Capstone", false, R.drawable.default_photo));
+        things.add(new Thing("Things", false, R.drawable.default_photo));
     }
 
     private void initializeAdapter(){
         RVAdapter adapter = new RVAdapter(things);
         rv.setAdapter(adapter);
+    }
+
+    private void initFab(){
+        findViewById(R.id.fab_settings).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                things.add(new Thing("HomeNect", false, R.drawable.default_photo));
+                initializeAdapter();
+            }
+        });
     }
 }
--- myungho/HomeNect/app/src/main/java/com/capstone/sejong/homenect/RVAdapter.java
+++ myungho/HomeNect/app/src/main/java/com/capstone/sejong/homenect/RVAdapter.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -15,9 +16,9 @@ import java.util.List;
  * Created by 12aud on 2017-03-31.
  */
 
-public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ThingViewHolder> {
+public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ThingViewHolder>{
 
-    public static class ThingViewHolder extends RecyclerView.ViewHolder{
+    public class ThingViewHolder extends RecyclerView.ViewHolder{
         CardView cv;
         TextView thingName;
         Switch status;
@@ -29,8 +30,20 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ThingViewHolder> {
             thingName = (TextView)itemView.findViewById(R.id.thing_name);
             status = (Switch)itemView.findViewById(R.id.thing_status);
             thingPhoto = (ImageView)itemView.findViewById(R.id.thing_photo);
+            thingPhoto.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if((Integer)thingPhoto.getTag() == R.drawable.default_photo){
+                        Toast.makeText(v.getContext(), "ITEM PRESSED = " + String.valueOf(getAdapterPosition()), Toast.LENGTH_SHORT).show();
+                    } else{
+                        // 사진 찍어서 넣는 곳
+                        Toast.makeText(v.getContext(), "있던 사진 눌린겨", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
         }
     }
+
     List<Thing> things;
 
     RVAdapter(List<Thing> things){
@@ -54,10 +67,12 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ThingViewHolder> {
         thingViewHolder.thingName.setText(things.get(i).name);
         thingViewHolder.status.setChecked(things.get(i).status);
         thingViewHolder.thingPhoto.setImageResource(things.get(i).photoId);
+        thingViewHolder.thingPhoto.setTag(things.get(i).photoId);
     }
 
     @Override
     public int getItemCount(){
         return things.size();
     }
+
 }
